737a7d2e54f9899ecb75f0fca1682d4225fc34dd
Jay <jaywhang83@gmail.com>
2016-03-25 07:50:39 -0700

    Add initial set up

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+// defining annotation aka "decorator"
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+  </div>
+  ` 
+})
+
+export class AppComponent {
+
+}
